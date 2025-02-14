d71133191ee84db374ea8cccff968064c5ad1cbe
yKaayo <caiokaea@gmail.com>
2025-02-14 16:11:22 -0300

    Parte 02

--- restaurant.py
+++ restaurant.py
@@ -1,16 +1,4 @@
-class Restaurant:
-    name = ''
-    category = ''
-    active = False
-    
-pizzaDoZe = Restaurant()
-pizzaDoZe.name = 'Pizza do Ze'
-pizzaDoZe.category = 'Pizzaria'
-
-print(vars(pizzaDoZe))
-print(pizzaDoZe.name)
-
-# ----------------
+# ----------
 
 class Music:
     name = ''
@@ -21,3 +9,25 @@ trapMusic = Music()
 trapMusic.name = 'Massive'
 trapMusic.artist = 'Drake'
 trapMusic.duration = 200
+
+# ----------
+
+class Restaurant:
+    restaurants = []
+    
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        self.active = False
+        Restaurant.restaurants.append(self)
+        
+    def __str__(self):
+        return f'{self.name}, {self.category}'
+    
+    def show_restaurant():
+        for restaurant in Restaurant.restaurants:
+            print(f'{restaurant.name}, {restaurant.category}, {restaurant.active}')
+    
+pizzaDoZe = Restaurant('Pizza do Ze', 'Pizzaria')
+
+Restaurant.show_restaurant()
